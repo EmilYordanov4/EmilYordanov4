0484b4e46005ce741daf8d22464eb3959cdfbb72
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-01 21:10:30 +0300

    Add files via upload

---
+++ Programming Basics/Conditional Statements/AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double face = side * side;
+                Console.WriteLine(Math.Round(face, 3));
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double face = sideA * sideB;
+                Console.WriteLine(Math.Round(face, 3));
+            }
+            else if (figure == "circle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double face = (side * side) * Math.PI;
+                Console.WriteLine(Math.Round(face, 3));
+            }
+            else 
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double face = (sideA * sideB) / 2;
+                Console.WriteLine(Math.Round(face ,3));
+            }
+        }
+    }
+}
---
+++ Programming Basics/Conditional Statements/ToyShop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double puzzlePrice = 2.60;
+            double dollPrice = 3;
+            double bearPrice = 4.10;
+            double minionPrice = 8.20;
+            double truckPrice = 2;
+
+            double tripPrice = double.Parse(Console.ReadLine());
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            double sum = puzzles * puzzlePrice + dolls * dollPrice +
+                bears * bearPrice + minions * minionPrice + trucks * truckPrice;
+            double toys = puzzles + dolls + bears + minions + trucks;
+            if (toys >= 50)
+            {
+                double profitWithSale = sum - sum * 0.25;
+                double profit = profitWithSale - profitWithSale * 0.10;
+
+                if (profit >= tripPrice)
+                {
+                    double remain = profit - tripPrice;
+                    //Console.WriteLine($"Yes! {Math.Round(remain, 2)} lv left.");
+                    Console.WriteLine($"Yes! {remain,0:F2} lv left.");
+                }
+                else
+                {
+                    double needed = tripPrice - profit;
+                    Console.WriteLine($"Not enough money! {needed,0:F2} lv needed.");
+                }
+
+            }
+            else
+            {
+                double profit = sum - sum * 0.10;
+                if (profit > tripPrice)
+                {
+                    double remain = profit - tripPrice;
+                    Console.WriteLine($"Yes! {remain,0:F2} lv left.");
+                }
+                else
+                {
+                    double needed = tripPrice - profit;
+                    Console.WriteLine($"Not enough money! {needed,0:F2} lv needed.");
+                }
+            }
+        }
+    }
+}
