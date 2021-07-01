0fd06fd739bd4eaf63d8e3ea39b74000baad832d
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-01 21:04:58 +0300

    Add files via upload

---
+++ Programming Basics/First Steps In Coding/VacationBooksList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double readTime = pages / pagesPerHour;
+            double hoursPerDay = readTime / days;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
