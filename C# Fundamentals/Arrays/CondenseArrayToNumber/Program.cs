1712f55c418f2af5393c62742a254c14c6986b86
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-02 12:16:03 +0300

    Add files via upload

---
+++ C# Fundamentals/Arrays/CondenseArrayToNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensed = new int[arrayOfNumbers.Length - 1];
+
+
+            if (arrayOfNumbers.Length == 1)
+            {
+                Console.WriteLine(arrayOfNumbers[0]);
+                return;
+
+            }
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                for (int j = 0; j < condensed.Length - i; j++)
+                {
+                    condensed[j] = arrayOfNumbers[j] + arrayOfNumbers[j + 1];
+                }
+                arrayOfNumbers = condensed;
+            }
+            Console.WriteLine(condensed[0]);
+        }
+    }
+}
+
+//for (int i = 0; i < arrayOfNumbers.Length; i++)
+//{
+//
+//}
