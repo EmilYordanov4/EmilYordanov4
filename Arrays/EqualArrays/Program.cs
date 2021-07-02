cf8d617c382e43c486a455cdc129b721695eb8cc
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-02 12:14:39 +0300

    Add files via upload

---
+++ Arrays/EqualArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(' ').ToArray();
+            string[] secondArray = Console.ReadLine().Split(' ').ToArray();
+
+            int index = 0;
+            bool equal = true;
+            int sum = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    int number = int.Parse(firstArray[i]);
+                    sum += number;
+                }
+                else
+                {
+                    equal = false;
+                    index = i;
+                }
+
+                if (equal == false)
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+                    break;
+                }
+            }
+            if (equal == true)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
---
+++ Arrays/SumEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ').ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = int.Parse(numbers[i]);
+
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
