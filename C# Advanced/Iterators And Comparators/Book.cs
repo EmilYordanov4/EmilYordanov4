343c015e081ef45c49271d2bbeabbfc40497b440
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-02 12:54:07 +0300

    Add files via upload

---
+++ C# Advanced/Iterators And Comparators/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+
+            if (this.Year == other.Year)
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+            else
+            {
+               return this.Year.CompareTo(other.Year);
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
