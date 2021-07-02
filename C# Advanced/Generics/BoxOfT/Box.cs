5b764cb8550c414d12e9b2b69a71bd2745b6b3cd
Emil Yordanov <86794524+EmilYordanov4@users.noreply.github.com>
2021-07-02 12:52:13 +0300

    Add files via upload

---
+++ C# Advanced/Generics/BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> BoxList { get; set; }
+        public int Count { get => BoxList.Count; }
+
+        public Box()
+        {
+            BoxList = new List<T>();
+        }
+        public void Add(T element)
+        {
+            BoxList.Add(element);
+        }
+        public T Remove()
+        {
+            var output = BoxList.LastOrDefault();
+            BoxList.RemoveAt(BoxList.Count - 1);
+            return output;
+        }
+    }
+}
